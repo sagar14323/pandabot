33072486f9f0150425a61db28000d2de12e06f90
rahul5544 <48445867+rahul5544@users.noreply.github.com>
2019-04-15 00:17:25 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = "*" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "567044799446908939" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "567044799446908939" && message.channel.id === "564496431227076667"){
         if(msg.startsWith('*', 0)){
-            if(msg === prefix + "hi" && message.channel.id === "381397780385628181"){
+            if(msg === prefix + "hi" && message.channel.id === "564496431227076667"){
                 message.channel.send('hello!!!!!') // Sends a message to the channel, with the content
             }
             else{
