e3eeb8e9f8aadc58bff9d8939297a8d48edfe141
rahul5544 <48445867+rahul5544@users.noreply.github.com>
2019-04-14 23:32:25 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = "*" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "567044799446908939" && message.channel.id === "381397780385628181"){
+        if(msg.startsWith('*', 0)){
+            if(msg === prefix + "hi" && message.channel.id === "381397780385628181"){
+                message.channel.send('hello!!!!!') // Sends a message to the channel, with the content
+            }
+            else{
+                message.channel.send("Hey, I only understand *hi right now. More will be added when my creator is free. :) ")
+            }
+        }
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '*' dot.");
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN)
